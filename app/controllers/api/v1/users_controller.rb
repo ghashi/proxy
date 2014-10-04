a2394ce1ee2d078d19446b814c62a6e81f82c30b
Guilherme Hashioka <memo@innku.com>
2014-10-04 00:16:25 -0300

    filter kind of request(get or post) before redirecting

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,9 +2,14 @@ require 'net/http'
 
 class Api::V1::UsersController < ApplicationController
   def redirect
-    data = params[:data]
-    url = URI.parse(URI.encode(data[:url]))
-    res = Net::HTTP.get_response(url)
+    url = URI.parse(URI.encode(params[:url]))
+    if params[:method].upcase == "GET"
+      res = Net::HTTP.get_response(url)
+    else
+      res = Net::HTTP.post_form(url, params[:params])
+    end
+
+    puts res['Content-Length']
 
     render html: res.body.html_safe
   end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -12,7 +12,12 @@ describe Api::V1::UsersController, type: :controller do
 
       it "should return get request response" do
         expect(requester).to receive(:get_response).with(URI.parse("http://en.wikipedia.org/wiki/Cassius_(band)")).and_return(request_response)
-        post :redirect, data: {url: "http://en.wikipedia.org/wiki/Cassius_(band)"}
+        post :redirect, url: "http://en.wikipedia.org/wiki/Cassius_(band)", method: "get"
+      end
+
+      it "should return post request response" do
+        expect(requester).to receive(:post_form).with(URI.parse("http://example.com/user"), {"name" => "Gui", "age" => "23"}).and_return(request_response)
+        post :redirect, url: "http://example.com/user", method: "post", params: {name: "Gui", age: 23}
       end
     end
   end
